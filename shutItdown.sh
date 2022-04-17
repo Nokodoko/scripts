de4f30c321dfa2b6c72d47b5ac9b38e4263fd5dd
chris <chris.montgomery@capacity.com>
2022-04-17 13:31:33 -0400

    updated scripts

---
+++ shutItdown.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+#variables
+RC=$?
+ns=notify-send 
+
+#shutting down the computer
+${ns} "Brb!"
+echo "kmonad ~/.config/kmonad/config.kbd &" >> ~/.zshenv 
+sleep .5
+shutdown -h now
+
+#testing if it's still alive
+if [ ${RC} -eq 0 ]; then
+    continue
+else
+    ${ns} "I'm still here!"
+fi
